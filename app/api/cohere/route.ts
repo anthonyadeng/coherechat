7dad721e15dedde27e46fe23dbf661c4c2e37f6c
Anthony Deng <anthonyadeng@gmail.com>
2024-04-30 22:18:38 -0400

    test

--- app/api/cohere/route.ts
+++ app/api/cohere/route.ts
@@ -7,3 +7,33 @@ export async function POST(request: NextRequest) {
   const res = await chat(message, chatHistory);
   return Response.json({ res });
 }
+
+// import fetch from 'node-fetch';
+// import fs from 'fs';
+
+// const formData = new FormData();
+// formData.append('mode', 'text-to-image');
+// formData.append('prompt', 'afd, dffsgs, ');
+// formData.append('aspect_ratio', '1:1');
+// formData.append('output_format', 'png');
+// formData.append('model', 'sd3');
+
+// (async () => {
+//   const response = await fetch(
+//     'https://api.stability.ai/v2beta/stable-image/generate/sd3',
+//     {
+//       method: 'POST',
+//       headers: {
+//         Accept: 'image/*',
+//         Authorization: 'Bearer sk-<STABILITY_API_KEY>',
+//       },
+//       body: formData,
+//     }
+//   );
+
+//   const buffer = await response.arrayBuffer();
+
+//   fs.writeFile('afd,.png', Buffer.from(buffer), () =>
+//     console.log('finished downloading!')
+//   );
+// })().catch(console.error);
